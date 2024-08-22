37fdd0130895cf3f59bf21cad2205183b5c15072
Yuriy Bandura <bandurayv@gmail.com>
2024-08-22 18:53:03 +0300

    Add_My_Car

---
+++ main.py
@@ -0,0 +1,27 @@
+class Engine():
+
+    def start(self):
+
+        print("Двигатель запущен")
+
+    def stop(self):
+
+        print("Двигатель остановлен")
+
+class Car():
+
+    def __init__(self):
+
+        self.engine = Engine()
+
+    def start(self):
+
+        self.engine.start()
+
+    def stop(self):
+
+        self.engine.stop()
+
+car = Car()
+car.start()
+car.stop()
