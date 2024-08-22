ce8f4c36f966f9e0fcbb46020d690798f9beb77a
Yuriy Bandura <bandurayv@gmail.com>
2024-08-22 19:42:11 +0300

    Add_Shape

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
-class Animal:
-    def make_sound(self):
-        pass
+class Shape():
+    def area(self):
+        return 0
 
-class Dog(Animal):
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+    def area(self):
+        return 3.14 * self.radius * self.radius
 
-    def make_sound(self):
-        print("гав")
+class Rectangle(Shape):
+    def __init__(self, length, breadth):
+        self.length = length
+        self.breadth = breadth
+    def area(self):
+        return self.length * self.breadth
 
-class Cat(Animal):
-    def make_sound(self):
-        print("мяу")
+class Square(Shape):
+    def __init__(self, width):
+        self.width = width
 
-class Cow(Animal):
-    def make_sound(self):
-        print("мууу")
+    def area(self):
+        return self.width ** 2
+
+def printArea(shape):
+    print(f'Площадь {type(shape).__name__} равна {shape.area()}')
 
 
-animals = [Dog(), Cat(), Cow()]
+c = Circle(10)
+r = Rectangle(10, 20)
+sq = Square(7)
 
-for animal in animals:
-    animal.make_sound()
+
+printArea(c)
+printArea(r)
+printArea(sq)
